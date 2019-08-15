bd10efbb8ea6b30c454e01624a30c92153ae8808
vinayski <ski@sankhe.com>
2019-08-15 14:31:49 -0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -15,13 +15,6 @@
 
 <main>
   <h1>Svelte + Node.js + Python API</h1>
-  <h2>
-    Deployed with
-    <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
-      ZEIT Now
-    </a>
-    !
-  </h2>
   <p>
     <a
       href="https://github.com/vinayski/svelte-functions"
@@ -29,22 +22,17 @@
       rel="noreferrer noopener">
       This project
     </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
+    is contains a serverless
     <a href="https://nodejs.org/en/">Node.js</a>
     function. See
     <a href="/api/date">
       <code>api/date</code>
       for the Date API with Node.js
     </a>
-    .
+    <a href="/apip/date">
+      <code>apip/date</code>
+      & with Python
+    </a>.
   </p>
   <br />
   <h2>The date according to Node.js is:</h2>
