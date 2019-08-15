1d67f62c3bac51bfabb084cd667859962c78f003
vinayski <ski@sankhe.com>
2019-08-15 13:15:08 -0700

    show date from python api call

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <script>
   import { onMount } from "svelte";
   export let date;
+  export let datep;
 
   onMount(async () => {
     const res = await fetch("/api/date");
+    const resp = await fetch("/api/datep");
     const newDate = await res.text();
+    const newDatep = await resp.text();
     date = newDate;
+    datep = newDatep;
   });
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
+  <h1>Svelte + Node.js + Python API</h1>
   <h2>
     Deployed with
     <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
@@ -45,4 +49,6 @@
   <br />
   <h2>The date according to Node.js is:</h2>
   <p>{date ? date : 'Loading date...'}</p>
+  <h2>The date from python api is:</h2>
+  <p>{datep ? datep : 'Loading date...'}</p>  
 </main>
